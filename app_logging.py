cce1a1a1cb79adb3695a42c83d0158a77f5be8a9
Chuck Kring <cjkring@MacBook-Pro.local>
2021-03-17 12:09:46 -0700

    add app_logging.py

---
+++ app_logging.py
@@ -0,0 +1,25 @@
+import logging
+from logging.handlers import WatchedFileHandler
+import sys
+
+def test_logging():
+    root = logging.getLogger('ifxgui')
+    root.setLevel(logging.DEBUG)
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    root.addHandler(handler)
+
+def app_logging(config,level):
+    root = logging.getLogger('ifxgui')
+    root.setLevel(level)
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+
+    handler = WatchedFileHandler(config['app']['logfile'])
+    handler.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
